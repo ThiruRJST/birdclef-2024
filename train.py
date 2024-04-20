a7b0d69b3e9f994bc9d8eca85cddc094076e07b8
ThiruRJST <maroonbleed007@gmail.com>
2024-04-20 12:07:00 +0530

    best model save added

--- train.py
+++ train.py
@@ -55,11 +55,12 @@ def train(epoch, trainloader, train_loss, optimizer):
     return train_epoch_loss
 
 
-def val(epoch, valloader, val_loss):
+def val(epoch, valloader, val_loss, rocauc):
 
     model.eval()
     pbar = tqdm(enumerate(valloader), total=len(valloader))
     running_test_loss = 0.0
+    running_rocauc = 0.0
 
     for i, (image, label) in pbar:
         image = image.to("cuda:0").float()
@@ -68,10 +69,13 @@ def val(epoch, valloader, val_loss):
         with torch.no_grad():
             outputs = model(image)
         loss = val_loss(outputs, label)
+        auc = rocauc(preds=outputs, target=label)
         running_test_loss += loss.item()
+        running_rocauc += auc.item()
     
     test_epoch_loss = running_test_loss / len(valloader)
-    return test_epoch_loss
+    test_epoch_auc = running_rocauc / len(valloader)
+    return test_epoch_loss, test_epoch_auc
 
 if __name__ == "__main__":
     print(f"These are the Hyperparameter you have used: {HyperParams()}")
@@ -115,18 +119,24 @@ if __name__ == "__main__":
 
     train_loss = nn.CrossEntropyLoss()
     val_loss = nn.CrossEntropyLoss()
-    
+    rocauc = MulticlassROC(num_classes=HyperParams.num_classes, average="macro")
     optimizer = optim.Adam(model.parameters(), lr=HyperParams.lr)
 
+    best_auc = 0.0
+
+    os.makedirs(f"Best_models", exist_ok=True)
+
     for e in range(HyperParams.epochs):
+
 
         print("***** Training *****")
         train_epoch_loss = train(epoch=e, trainloader=train_loader, train_loss=train_loss, optimizer=optimizer)
 
         print("***** Testing *****")
-        test_epoch_loss = val(epoch=e, valloader=test_loader, val_loss=val_loss)
+        test_epoch_loss, test_epoch_auc = val(epoch=e, valloader=test_loader, val_loss=val_loss)
 
-        print(f"Epoch: {e} || Train Loss: {train_epoch_loss} || Test Loss: {test_epoch_loss}")
+        print(f"Epoch: {e} || Train Loss: {train_epoch_loss:.4f} || Test Loss: {test_epoch_loss:.4f} || Test AUC: {test_epoch_auc:.4f}")
 
-    
-
+        if test_epoch_auc > best_auc:
+            torch.save(model.state_dict(), f"Best_models/{HyperParams.Arch}_auc_{test_epoch_auc:.4f}.pth")
+            best_auc = test_epoch_auc
